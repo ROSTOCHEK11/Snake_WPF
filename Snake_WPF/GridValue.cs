cccca2333409d849575431820acc41fff033e623
Rostyslav Zholudiev <113441489+ROSTOCHEK11@users.noreply.github.com>
2024-01-09 22:16:24 +0100

    Add files via upload

---
+++ Snake_WPF/GridValue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_WPF
+{
+    public enum GridValue
+    {
+
+        Empty,
+        Snake,
+        Food,
+        Outside
+
+    }
+}
